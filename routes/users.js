3efa028e9955a3836ff645215fe6eef44df60dbb
sansela <santosh.sela@gmail.com>
2022-06-21 01:06:43 +0530

    initial commit for dev

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {getUsers, getUser, createUser, deleteUser, updateUser} from '../controllers/users.js'
+
+const router = express.Router();
+
+router.get('/', getUsers);
+router.get('/:id', getUser);
+router.post('/', createUser);
+router.delete('/:id', deleteUser);
+router.patch('/:id', updateUser);
+
+export default router;
